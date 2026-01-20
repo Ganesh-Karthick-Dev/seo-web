62367ac34f4e682b62373190c3a5948d8fbb8c0d
Ganesh Karthick <156606861+Ganesh-Karthick-Dev@users.noreply.github.com>
2026-01-20 22:22:46 +0530

    google tag added done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit, Bebas_Neue } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { FloatingHeader } from "@/components/ui/floating-header";
 import { Footer } from "@/components/sections/Footer";
@@ -37,9 +38,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K66G34JR');`,
+          }}
+        />
+      </head>
       <body
         className={`${outfit.variable} ${bebasNeue.variable} font-sans antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K66G34JR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <LenisProvider>
           <FloatingHeader />
           {children}
